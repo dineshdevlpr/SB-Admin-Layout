be22b77a53f92764cba26d13bf0b757a803040f3
dineshdevlpr <dineshiaf30@gmail.com>
2021-05-31 06:26:52 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/sideBar';
+import Topbar from './components/topBar'
+import Dashboard from './components/dashboard';
+import Card from './components/card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="wrapper">
+        <Sidebar/>
+        <div id="content-wrapper" class="d-flex flex-column"> 
+            <div id="content">
+              <Topbar/>
+              <div class="container-fluid">
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                  <Dashboard/>
+                </div>
+                <div class="row">
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                  </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </>
   );
 }
 
